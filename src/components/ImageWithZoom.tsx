be9e08b0842384395e09919d1dd1ec12e6eeb2c5
aelmanaa <aelmanaa@gmail.com>
2023-04-20 20:05:04 +0200

    solve ethernaut level3

---
+++ src/components/ImageWithZoom.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import ZoomImage from "./ZoomImage";
+
+interface ImageWithZoomProps {
+  src: string;
+  alt: string;
+}
+
+export default function ({ src, alt }: ImageWithZoomProps) {
+  const [showZoom, setShowZoom] = useState(false);
+
+  const toggleZoom = () => {
+    console.log("togglezoom");
+    setShowZoom(!showZoom);
+  };
+
+  const styles = `
+    .image-container {
+      position: relative;
+      cursor: pointer;
+    }
+  `;
+
+  return (
+    <>
+      <style>{styles}</style>
+      <div className="image-container" onClick={toggleZoom}>
+        <img src={src} alt={alt} />
+        {showZoom && <ZoomImage src={src} alt={alt} onClose={toggleZoom} />}
+      </div>
+    </>
+  );
+}
---
+++ src/scripts/index.ts
@@ -0,0 +1,1 @@
+import "./click-to-zoom";
